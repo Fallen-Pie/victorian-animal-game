1a2d13e7c727a65d08736b406df1d5df7b2d8443
Blake Davis <brickanimator@gmail.com>
2025-06-30 04:16:12 +1200

    Created Province Classes

---
+++ Scripts/Province/LandProvince.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+
+namespace VictorianAnimalGame.Scripts.Province
+{
+    public class LandProvince : IProvince
+    {
+
+    }
+}
---
+++ Scripts/Province/SeaProvince.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VictorianAnimalGame.Scripts.Province
+{
+    internal partial class SeaProvince : Node, IProvince
+    {
+
+    }
+}
