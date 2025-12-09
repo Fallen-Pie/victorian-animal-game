21f5e706089e58ae8c71e3d5c44bc786c9968874
Blake Davis <brickanimator@gmail.com>
2025-12-09 20:32:06 +1300

    Added Critter Entry

---
+++ Scripts/Critters/CritterEntry.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace VictorianAnimalGame.Scripts.Critters
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly record struct CritterEntry
+    {
+        private readonly CritterCulture _culture;
+        private readonly CritterSpecies _species;
+        private readonly CritterOccupation _occupation;
+        private readonly CritterDetails _critterDetails;
+
+        public CritterEntry(CritterSpecies newSpecies, CritterOccupation newOccupation, 
+            CritterCulture newCulture, CritterDetails newDetails)
+        {
+            _species = newSpecies;
+            _occupation = newOccupation;
+            _culture = newCulture;
+            _critterDetails = newDetails;
+        }
+
+        public CritterDetails GetCritterDetails()
+        {
+            return _critterDetails;
+        }
+        
+        public bool Equals(CritterEntry newCritter) =>
+            (_culture, _species, _occupation).Equals(
+                (newCritter._culture, newCritter._species, newCritter._occupation));
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_culture, _species, _occupation);
+        }
+
+        public override string ToString()
+        {
+            return $"Info on this {_species}: " +
+                   $"{_culture}" +
+                   $"/{_occupation}" +
+                   $"/{GetHashCode()}/{_critterDetails}";
+        }
+    }
+}
