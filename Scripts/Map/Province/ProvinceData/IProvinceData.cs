d6fec8ff9f0b1e366af6a1784d61058dc54c1316
Blake Davis <brickanimator@gmail.com>
2025-12-11 23:02:54 +1300

    Added More ProvinceData gathering methods

--- Scripts/Map/Province/ProvinceData/IProvinceData.cs
+++ Scripts/Map/Province/ProvinceData/IProvinceData.cs
@@ -1,40 +1,7 @@
-﻿using System;
-using VictorianAnimalGame.Scripts.Critters;
-
-namespace VictorianAnimalGame.Scripts.Map.Province.ProvinceData;
+﻿namespace VictorianAnimalGame.Scripts.Map.Province.ProvinceData;
 
 public interface IProvinceData
 {
+    public int GetHashCode();
     public void AddAmount(uint newAmount);
-}
-
-public record WorkforceData : IProvinceData
-{
-    public WorkforceData(CritterOccupation newOccupations, uint newCount)
-    {
-        Occupations = newOccupations;
-        Amount = newCount;
-    }
-
-    private CritterOccupation Occupations { get; }
-    private uint Amount { get; set; }
-    
-    public virtual bool Equals(WorkforceData newCritter) =>
-        Occupations == newCritter.Occupations;
-
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Occupations);
-    }
-
-    public override string ToString()
-    {
-        return $"{Occupations} workforce: {Amount}";
-    }
-
-    public void AddAmount(uint newAmount)
-    {
-        Amount += newAmount;
-    }
 }
