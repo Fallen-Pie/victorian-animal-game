8df934824d44edc0c3ca87cbae0af02080f6299f
Blake Davis <brickanimator@gmail.com>
2025-12-15 19:19:06 +1300

    Implemented Species Defines

--- Scripts/Critters/Species/Species.cs
+++ Scripts/Critters/Species/Species.cs
@@ -1,11 +1,31 @@
 ﻿namespace VictorianAnimalGame.Scripts.Critters.Species;
 
-public class Species
+public readonly record struct Species
 {
-    private readonly float _foodConsumption = 1;
-    private readonly float _workforceValue = 1;
+    private readonly CritterSpecies _speciesType;
+    private readonly float _foodConsumption;
+    private readonly float _workforceValue;
+    //private readonly Consumption _speciesConsumption;
+
+    public Species(CritterSpecies species, float foodConsumption, float workforce)
+    {
+        _speciesType = species;
+        _foodConsumption = foodConsumption;
+        _workforceValue = workforce;
+    }
+
+    public float GetWorkforce()
+    {
+        return _workforceValue;
+    }
+
+    public override string ToString()
+    {
+        return $"Name:{_speciesType}|Food:{_foodConsumption}|Work:{_workforceValue}";
+    }
+}
+
+public class Consumption
+{
     
-    private string _speciesName;
-    private CritterSpecies _critterSpecies;
-    //private list<Goods>  _goods;
 }
---
+++ Scripts/Defines/Defines.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Frozen;
+using System.Collections.Generic;
+using VictorianAnimalGame.Scripts.Critters;
+using VictorianAnimalGame.Scripts.Critters.Species;
+
+namespace VictorianAnimalGame.Scripts.Defines;
+
+public static class CritterDefines
+{
+    public static readonly FrozenDictionary<CritterSpecies, Species> Species = CreateFrozenDictionary();
+    
+    private static FrozenDictionary<CritterSpecies, Species> CreateFrozenDictionary()
+    {
+        Dictionary<CritterSpecies, Species> species = [];
+        species.Add(CritterSpecies.Otter, new Species(CritterSpecies.Otter, 1, 1.1f));
+        species.Add(CritterSpecies.Beaver, new Species(CritterSpecies.Beaver, 1, 0.9f));
+        species.Add(CritterSpecies.Raccoon, new Species(CritterSpecies.Raccoon, 1, 1));
+        return species.ToFrozenDictionary();
+    }
+}
