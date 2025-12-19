de5c36b8c81a75357450d8e130405c99556a59c8
Blake Davis <brickanimator@gmail.com>
2025-12-19 23:48:25 +1300

    Initial Food Types

---
+++ Scripts/Goods/Food/FoodTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VictorianAnimalGame.Scripts.Goods.Food;
+
+[Flags]
+public enum FoodTypes
+{
+    Meat = 0b_0000_0001,
+    Fish = 0b_0000_0010,
+    Fruits = 0b_0000_0100,
+    Vegetables = 0b_0000_1000,
+    Tubers = 0b_0001_0000,
+    Dairy = 0b_0010_0000,
+    Honey = 0b_0100_0000,
+    Nuts = 0b_1000_0000,
+    
+    Carnivore = Meat | Fish,
+    Vegan = Fruits | Vegetables | Tubers | Nuts,
+    Vegetarian = Vegan | Dairy | Honey,
+    Pescatarian = Fish | Vegetarian,
+    Omnivore = Carnivore | Vegetarian
+}
