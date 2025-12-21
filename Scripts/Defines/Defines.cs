518d9167283032d58027c518160b23bd6991bac3
Blake Davis <brickanimator@gmail.com>
2025-12-22 02:30:23 +1300

    Added MapDefines for Year

--- Scripts/Defines/Defines.cs
+++ Scripts/Defines/Defines.cs
@@ -8,19 +8,37 @@ namespace VictorianAnimalGame.Scripts.Defines;
 public static class CritterDefines
 {
     public static readonly FrozenDictionary<CritterSpecies, Species> Species = CreateFrozenSpecies();
-    public static readonly FrozenDictionary<CritterSpecies, Species> Goods = CreateFrozenGoods();
-
-    private static FrozenDictionary<CritterSpecies, Species> CreateFrozenGoods()
-    {
-        throw new System.NotImplementedException();
-    }
+    // public static readonly FrozenDictionary<CritterSpecies, Species> Goods = CreateFrozenGoods();
+    //
+    // private static FrozenDictionary<CritterSpecies, Species> CreateFrozenGoods()
+    // {
+    //     throw new System.NotImplementedException();
+    // }
 
     private static FrozenDictionary<CritterSpecies, Species> CreateFrozenSpecies()
     {
         Dictionary<CritterSpecies, Species> species = [];
-        species.Add(CritterSpecies.Otter, new Species(CritterSpecies.Otter, 1, 1.1f));
-        species.Add(CritterSpecies.Beaver, new Species(CritterSpecies.Beaver, 1, 0.9f));
-        species.Add(CritterSpecies.Raccoon, new Species(CritterSpecies.Raccoon, 1, 1));
+        species.Add(CritterSpecies.Otter, new Species(CritterSpecies.Otter, 1, 1.1f,
+            5, 10, 15));
+        species.Add(CritterSpecies.Beaver, new Species(CritterSpecies.Beaver, 1, 0.9f,
+            5, 10, 15));
+        species.Add(CritterSpecies.Raccoon, new Species(CritterSpecies.Raccoon, 1, 1, 
+            5, 10, 15));
         return species.ToFrozenDictionary();
+    }
+}
+
+public static class MapDefines
+{
+    private static short _currentYear = 1819;
+        
+    public static short GetCurrentYear()
+    {
+        return _currentYear;
+    }
+
+    private static void IncreaseCurrentYear()
+    {
+        _currentYear += 1;
     }
 }
