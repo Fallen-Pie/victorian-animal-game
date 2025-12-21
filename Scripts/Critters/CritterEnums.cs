61b8f7ea09f104df05566f99af2e54ccece4a171
Blake Davis <brickanimator@gmail.com>
2025-12-21 22:25:57 +1300

    Updated CritterOccupation

--- Scripts/Critters/CritterEnums.cs
+++ Scripts/Critters/CritterEnums.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace VictorianAnimalGame.Scripts.Critters
 {
     public enum CritterSpecies : ushort
@@ -8,29 +10,42 @@ namespace VictorianAnimalGame.Scripts.Critters
         Raccoon
     }
 
-    public enum CritterClass : byte
-    {
-        UPPER,
-        MIDDLE,
-        LOWER,
-        NONE
-    }
-
-    public enum CritterOccupation : ushort
-    {
-        None = 0,
-        Dependent,
-        Labourer,
-        Worker,
-        Aristocrats,
-        Bureaucrats
-    }
-    
     public enum CritterCulture : ushort
     {
         None = 0,
-        OtterAmericans,
-        BeaverAmericans,
-        RaccoonAmericans
+        Dutch,
+        Americans,
+        British,
+        Irish
+    }
+    
+    [Flags]
+    public enum CritterOccupation : ushort
+    {
+        Dependent = 0,
+        Manager = 0b_0000_0001,
+        Student = 0b_0000_0010,
+        Conscript = 0b_0000_0100,
+        Labourer = 0b_0000_1000,
+        Farmer = 0b_0001_0000,
+        Artisan = 0b_0010_0000,
+        Worker = 0b_0100_0000,
+        Ruler = 0b_1000_0000,
+        Administrator = 0b_0001_0000_0000,
+        Regular = 0b_0010_0000_0000,
+        Officer = 0b_0100_0000_0000,
+        Scholar = 0b_1000_0000_0000,
+    
+        Labour = Labourer | Farmer | Artisan | Worker,
+        Clerk = Ruler | Administrator | Scholar | Manager,
+        Solider = Conscript | Regular | Officer,
+    
+        Young = Dependent,
+        Adolescent = Student | Worker | Dependent,
+        Adult = Clerk | Labour | Solider | Dependent,
+    
+        LowerClass = Labourer | Farmer | Worker | Conscript | Student,
+        MiddleClass = Administrator | Scholar | Regular | Artisan,
+        UpperClass = Officer | Ruler | Manager,
     }
 }
