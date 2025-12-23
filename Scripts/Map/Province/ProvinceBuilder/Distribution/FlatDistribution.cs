08d9b6926ddc1361387c3b9f9cf8ff5dc44b177f
Blake Davis <brickanimator@gmail.com>
2025-12-23 22:57:14 +1300

    Added CritterDistribution Strategy

---
+++ Scripts/Map/Province/ProvinceBuilder/Distribution/FlatDistribution.cs
@@ -0,0 +1,15 @@
+﻿using VictorianAnimalGame.Scripts.Critters.Species;
+
+namespace VictorianAnimalGame.Scripts.Map.Province.ProvinceBuilder.Distribution;
+
+public class FlatDistribution : ICritterDistribution
+{
+    public double Execute(short age, Species species)
+    {
+        if (age <= species.FertileAge)
+        {
+            return 1.0 - (0.2 * ((double)age / species.FertileAge)); 
+        }
+        return 0.8 * (1.0 - (double)(age - species.FertileAge) / (species.ElderAge - species.FertileAge));
+    }
+}
