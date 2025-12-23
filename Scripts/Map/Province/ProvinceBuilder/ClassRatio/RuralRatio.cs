cce691ed60503b84e9fc904b571c3d9417eef19f
Blake Davis <brickanimator@gmail.com>
2025-12-23 22:57:30 +1300

    Added ProvinceRatio Strategy

---
+++ Scripts/Map/Province/ProvinceBuilder/ClassRatio/RuralRatio.cs
@@ -0,0 +1,12 @@
+﻿namespace VictorianAnimalGame.Scripts.Map.Province.ProvinceBuilder.ClassRatio;
+
+public class RuralRatio : IClassRatio
+{
+    public (uint Lower, uint Middle, uint Upper) Execute(uint count)
+    {
+        uint upperCount = (uint)(count * 0.03);
+        uint middleCount = (uint)(count * 0.12);
+        count = count - upperCount - middleCount;
+        return (count, middleCount, upperCount);
+    }
+}
