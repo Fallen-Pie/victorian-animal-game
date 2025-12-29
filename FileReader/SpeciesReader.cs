66d0bd8d22090cdc4daa46928827626acf81a1a2
Blake Davis <brickanimator@gmail.com>
2025-12-30 04:48:39 +1300

    Intial SpeciesReader.cs

---
+++ FileReader/SpeciesReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using YamlDotNet.Serialization;
+
+namespace VictorianAnimalGame.FileReader;
+
+public static class SpeciesReader
+{
+    public static void ReadSpeciesYaml(string filePath)
+    {
+        var input = new StreamReader(filePath);
+
+        var deserializer = new DeserializerBuilder().Build();
+        
+        var yamlObject = deserializer.Deserialize<dynamic>(input);
+        
+        Console.WriteLine($"{yamlObject["species"]["ages"]["adolescent"].ToString()}");
+    }
+}
+
