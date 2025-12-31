4b3fd6b9e4f1b5d6c88f9bd75f3b5c9e655867aa
Blake Davis <brickanimator@gmail.com>
2026-01-01 04:41:59 +1300

    Added SpeciesBuilder.cs

---
+++ Scripts/Critters/Species/SpeciesBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Godot.Collections;
+using VictorianAnimalGame.FileReader;
+
+namespace VictorianAnimalGame.Scripts.Critters.Species;
+
+public class SpeciesBuilder
+{
+    private sbyte _adolescentAge;
+    private sbyte _adultAge;
+    private sbyte _elderAge;
+    private sbyte _fertileAge;
+    
+    private SpeciesType _speciesType;
+    private float _foodConsumption;
+    private float _workforceValue;
+    
+    public SpeciesBuilder SetAges(AgeStages ages)
+    {
+        _adolescentAge = ages.Adolescent;
+        _adultAge = ages.Adult;
+        _elderAge = ages.Elder;    
+        return this; // Return the builder instance for chaining
+    }
+
+    public SpeciesBuilder SetSpeciesType(SpeciesType type)
+    {
+        _speciesType = type;
+        return this;
+    }
+    
+    public SpeciesBuilder SetPeakFertility(sbyte age)
+    {
+        _fertileAge = age;
+        return this;
+    }
+
+    public SpeciesBuilder SetFoodConsumption(float foodConsumption)
+    {
+         _foodConsumption = foodConsumption;
+        return this;
+    }
+    
+    public SpeciesBuilder SetWorkforceValue(float workforceValue)
+    {
+        _workforceValue = workforceValue;
+        return this;
+    }
+    
+    public Species Build()
+    {
+        return new Species(_speciesType, _foodConsumption, _workforceValue, 
+            _adolescentAge, _adultAge, _fertileAge, _elderAge);
+    }
+}
