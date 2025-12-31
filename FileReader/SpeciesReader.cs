9b007e43dd33d343cb3459e40d65512571dae02c
Blake Davis <brickanimator@gmail.com>
2025-12-31 23:02:01 +1300

    Added Initial File Reader

--- FileReader/SpeciesReader.cs
+++ FileReader/SpeciesReader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using VictorianAnimalGame.Scripts.Critters.Species;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace VictorianAnimalGame.FileReader;
 
@@ -8,13 +10,42 @@ public static class SpeciesReader
 {
     public static void ReadSpeciesYaml(string filePath)
     {
-        var input = new StreamReader(filePath);
+        var yamlInput = new StreamReader(filePath);
+        
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(PascalCaseNamingConvention.Instance)
+            .Build();
 
-        var deserializer = new DeserializerBuilder().Build();
-        
-        var yamlObject = deserializer.Deserialize<dynamic>(input);
-        
-        Console.WriteLine($"{yamlObject["species"]["ages"]["adolescent"].ToString()}");
+        var data = deserializer.Deserialize<SpeciesConfig>(yamlInput);
+
+        Console.WriteLine($"Species: {data.Species.Name}");
+        Console.WriteLine($"Food: {data.Species.WorkforceValue}");
+        Console.WriteLine($"Adult Age: {data.Species.Ages.Adult}");
     }
+}
+
+public class SpeciesConfig
+{
+    public SpeciesData Species { get; set; }
+}
+
+public class SpeciesData
+{
+    public string Name { get; set; }
+    
+    public double FoodConsumption { get; set; }
+    
+    public double WorkforceValue { get; set; }
+
+    public AgeStages Ages { get; set; }
+    
+    public int PeakFertility { get; set; }
+}
+
+public class AgeStages
+{
+    public int Adolescent { get; set; }
+    public int Adult { get; set; }
+    public int Elder { get; set; }
 }
 
