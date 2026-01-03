aa24a7a818f42525a10b367303b0a9217834ac45
Blake Davis <brickanimator@gmail.com>
2026-01-03 22:09:33 +1300

    Updated Critter Class Ratio

--- Scripts/Map/Province/ProvinceBuilder/ClassRatio/RuralRatio.cs
+++ Scripts/Map/Province/ProvinceBuilder/ClassRatio/RuralRatio.cs
@@ -4,9 +4,10 @@ public class RuralRatio : IClassRatio
 {
     public (uint Lower, uint Middle, uint Upper) Execute(uint count)
     {
-        uint upperCount = (uint)(count * 0.03);
-        uint middleCount = (uint)(count * 0.12);
-        count = count - upperCount - middleCount;
-        return (count, middleCount, upperCount);
+        uint lowerCount = count;
+        uint upperCount = (uint)(lowerCount * 0.03);
+        uint middleCount = (uint)(lowerCount * 0.12);
+        lowerCount = lowerCount - upperCount - middleCount;
+        return (lowerCount, middleCount, upperCount);
     }
 }
