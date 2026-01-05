0851241198f19d5edc89cef30eb7f80bd11f1d29
Blake Davis <brickanimator@gmail.com>
2026-01-05 15:10:47 +1300

    Started Culture Implimentation

--- Scripts/Critters/Cultures/CritterCulture.cs
+++ Scripts/Critters/Cultures/CritterCulture.cs
@@ -1,6 +1,11 @@
-﻿namespace VictorianAnimalGame.Scripts.Critters.Cultures;
+﻿using System;
+
+namespace VictorianAnimalGame.Scripts.Critters.Cultures;
 
 public class CritterCulture
 {
-    
+    private CultureType _cultureId;
+    private string CultureName;
+    private int CultureID;
+    private CulturalValue[] CultureValues;
 }
