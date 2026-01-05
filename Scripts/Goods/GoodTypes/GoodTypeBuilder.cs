7e919fcb97915f60c666e1ceb08e51c1461e71dd
Blake Davis <brickanimator@gmail.com>
2026-01-06 03:34:06 +1300

    Added GoodTypeBuilder.cs

---
+++ Scripts/Goods/GoodTypes/GoodTypeBuilder.cs
@@ -0,0 +1,31 @@
+﻿namespace VictorianAnimalGame.Scripts.Goods.GoodTypes;
+
+public class GoodTypeBuilder
+{
+    private string _goodName;
+    private float _price;
+    private float _bulk;
+    
+    public GoodTypeBuilder SetGoodName(string name)
+    {
+        _goodName = name;
+        return this;
+    }
+    
+    public GoodTypeBuilder SetGoodPrice(float price)
+    {
+        _price = price;
+        return this;
+    }
+    
+    public GoodTypeBuilder SetGoodBulk(float bulk)
+    {
+        _bulk = bulk;
+        return this;
+    }
+    
+    public GoodType Build()
+    {
+        return new GoodType(_goodName, _price, _bulk);
+    }
+}
