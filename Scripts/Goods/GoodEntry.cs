99336ae694efd664cb039e48ed27c1573aff962f
Blake Davis <brickanimator@gmail.com>
2026-01-06 03:33:50 +1300

    Added GoodEntry.cs (Needs additional fields)

---
+++ Scripts/Goods/GoodEntry.cs
@@ -0,0 +1,12 @@
+﻿using VictorianAnimalGame.Scripts.Goods.GoodTypes;
+
+namespace VictorianAnimalGame.Scripts.Goods;
+
+public struct GoodEntry
+{
+    private uint Count { get; set; }
+    private GoodType Type { get; init; }
+    //private Price TotalValue { get; init; }
+    
+    
+}
