9d8da6d470a77d915e0951c29dda4f7696df196b
Blake Davis <brickanimator@gmail.com>
2026-01-07 02:32:51 +1300

    Created GoodConfig File

---
+++ FileReader/DataConfig/GoodsConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace VictorianAnimalGame.FileReader.DataConfig;
+
+public class GoodsConfig : IDataConfig
+{
+    public GoodsData Goods { get; set; }
+}
+
+public class GoodsData
+{
+    public string Name { get; set; }
+    
+    public float Price { get; set; }
+    
+    public float Bulk { get; set; }
+}
