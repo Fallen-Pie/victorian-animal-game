1c3990b8273c0a4feebc40d22ad882562227ed4d
Blake Davis <brickanimator@gmail.com>
2026-01-06 19:49:11 +1300

    Added Strategy Pattern for YamlReader

---
+++ FileReader/ReaderStrategies/SpeciesStrategyReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using VictorianAnimalGame.FileReader.DataConfig;
+
+namespace VictorianAnimalGame.FileReader.ReaderStrategies;
+
+public class SpeciesStrategyReader : StrategyReader
+{
+    public override List<IDataConfig> ReadFiles(string fileLocation)
+    {
+        List<IDataConfig> speciesData = [];
+        
+        string[] speciesFiles = Directory.GetFiles(fileLocation, YamlReader.FileExtension);
+        
+        foreach (string file in speciesFiles)
+        {
+            IDataConfig newSpeciesData = (SpeciesConfig)DeserializerFile(file);
+            speciesData.Add(newSpeciesData);
+            
+            var console = (SpeciesConfig)newSpeciesData;
+            string debugString = $"Species: {console.Species.Name}|" +
+                    $"Food: {console.Species.WorkforceValue}|" +
+                    $"Adult Age: {console.Species.Ages.Adult}";
+            Console.WriteLine(debugString);
+        }
+
+        return speciesData;
+    }
+}
+
+
