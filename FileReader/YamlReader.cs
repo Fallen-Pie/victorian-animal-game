c20ffb5ccd98ac2314cafe7c4183aa0efd4efa93
Blake Davis <brickanimator@gmail.com>
2026-01-06 19:48:19 +1300

    Refactored Species Reader into YamlReader.cs

---
+++ FileReader/YamlReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using VictorianAnimalGame.FileReader.DataConfig;
+using VictorianAnimalGame.FileReader.ReaderStrategies;
+
+namespace VictorianAnimalGame.FileReader;
+
+public static class YamlReader
+{
+    public const string FileExtension = "*.yaml";
+    private static StrategyReader _strategyMethod;
+
+    public static void ChangeBehaviour(StrategyReader newStrategy)
+    {
+        _strategyMethod = newStrategy;
+    }
+        
+    public static List<IDataConfig> RunBehaviour(string fileLocation)
+    {
+        return _strategyMethod.ReadFiles(fileLocation);
+    }
+}
