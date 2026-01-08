c84e67aa5f0bf49f1d226b019745859446bc3722
Blake Davis <brickanimator@gmail.com>
2026-01-09 03:13:24 +1300

    Changed DateDefines.cs to use DateManager

--- Engine/Defines/DateDefines.cs
+++ Engine/Defines/DateDefines.cs
@@ -4,22 +4,24 @@ namespace VictorianAnimalGame.Engine.Defines;
 
 public static class DateDefines
 {
-    public const int PhasesPerDay = 6;
-    public const int DaysPerWeek = 7;
+    public const int PhasesAmount = 6;
+    public const int WeeklyDaysAmount = 7;
+    public const int MonthsAmount = 16;
+    public const int DaysPerMonth = 30;
     
-    public const int DaysPerYear = 500;
-    public const int MonthsPerYear = 16;
-    public const int DaysPerMonth = DaysPerYear / MonthsPerYear;
-    
-    public static TimeOfDay Phase = TimeOfDay.Dawn;
-    public static int Day = 1;
-    public static int Month = 1;
-    public static int Year = 1819;
+    //public const int DaysPerMonth = YearlyDaysAmount / MonthsAmount;
 
-    private static void IncreaseCurrentYear() => Year += 1;
-    private static void IncreaseCurrentMonth() => Month += 1;
-    private static void IncreaseCurrentDay() => Day += 1;
-    private static void IncreaseCurrentPhase() => Phase += 1;
+    private static readonly DateManager DateManager = new DateManager(DayPhase.Dawn, 1, 1, 1819);
+    
+    public static DayPhase Phase => DateManager.Phase;
+    public static int Day => DateManager.Day;
+    public static int Month => DateManager.Month;
+    public static int Year => DateManager.Year;
+
+    public static void IncrementTime()
+    {
+        DateManager.IncrementTime();
+    }
 
     public static string GetTime()
     {
