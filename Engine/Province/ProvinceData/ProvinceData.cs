980924b5cb6fee094f72f7a4d58dfc45a163e83a
Blake Davis <brickanimator@gmail.com>
2026-01-14 21:53:41 +1300

    Created ProvinceData.cs struct

---
+++ Engine/Province/ProvinceData/ProvinceData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using VictorianAnimalGame.Engine.Province.Critters;
+using VictorianAnimalGame.Engine.Province.Critters.Cultures;
+using VictorianAnimalGame.Engine.Province.Critters.Species;
+
+namespace VictorianAnimalGame.Engine.Province.ProvinceData;
+
+public record struct ProvinceData
+{
+    private CultureType Culture { get; }
+    private CritterLifeStage LifeStage { get; }
+    private SpeciesType Species { get; }
+    private CritterClass Class { get; }
+    public int Count { get; set; }
+
+    public ProvinceData(CultureType newCulture, CritterLifeStage newLifeStage, 
+        SpeciesType newSpecies, CritterClass newClass, int newCount)
+    {
+        Culture = newCulture;
+        LifeStage = newLifeStage;
+        Species = newSpecies;
+        Class = newClass;
+        Count = newCount;
+    }
+
+    public void AddCount(int newCount)
+    {
+        Count += newCount;
+    }
+    
+    public bool Equals(ProvinceData newData) =>
+        (_culture: Culture, _species: Species, _lifeStage: LifeStage, _class: Class).Equals(
+            (newData.Culture, newData.Species, newData.LifeStage, newData.Class));
+    
+    public override int GetHashCode() => HashCode.Combine(Culture, Species, LifeStage, Class);
+
+    public override string ToString()
+    {
+        return $"Current {Species}: {LifeStage}/{Class}/{Culture}/{Count}";
+    }
+}
