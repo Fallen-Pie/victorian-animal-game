0bdd4fbb3f8ddd94410fb8ff4a9cd8210a82528b
Blake Davis <brickanimator@gmail.com>
2026-01-22 01:18:10 +1300

    Added Struct fields for CritterDataGroup (CritterDataCounts.cs and CritterDataSpecial.cs)

---
+++ Engine/Province/ProvinceData/DataGroupParts/CritterDataCounts.cs
@@ -0,0 +1,13 @@
+﻿namespace VictorianAnimalGame.Engine.Province.ProvinceData.DataGroupParts;
+
+public struct CritterDataCounts
+{
+    public uint Dependants;
+    public uint Workers;
+    public uint Total => Dependants + Workers;
+
+    public override string ToString()
+    {
+        return $"({Dependants}.{Workers}.{Total})";
+    }
+}
---
+++ Engine/Province/ProvinceData/DataGroupParts/CritterDataSpecial.cs
@@ -0,0 +1,13 @@
+﻿namespace VictorianAnimalGame.Engine.Province.ProvinceData.DataGroupParts;
+
+public struct CritterDataSpecial
+{
+    public uint Students;
+    public uint Trained;
+    public uint Indisposed;
+
+    public override string ToString()
+    {
+        return $"({Students}.{Trained}.{Indisposed})";
+    }
+}
