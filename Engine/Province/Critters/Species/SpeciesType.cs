6753d7a2decb5810e6339d7d7c87ce8bfeaf44b3
Blake Davis <brickanimator@gmail.com>
2026-01-25 02:27:20 +1300

    Added Expression Bodied functions for SpeciesType.cs

--- Engine/Province/Critters/Species/SpeciesType.cs
+++ Engine/Province/Critters/Species/SpeciesType.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using VictorianAnimalGame.Engine.Defines;
 
 namespace VictorianAnimalGame.Engine.Province.Critters.Species;
 
 public readonly struct SpeciesType(ushort speciesId) : IEquatable<SpeciesType>
 {
     private ushort SpeciesId { get; } = speciesId;
+    
+    public string Name => CritterDefines.Species[this].SpeciesName;
+    public float Workforce => CritterDefines.Species[this].WorkforceValue;
 
     public override bool Equals(object obj) => obj is SpeciesType other && Equals(other);
     public bool Equals(SpeciesType other) => SpeciesId == other.SpeciesId;
