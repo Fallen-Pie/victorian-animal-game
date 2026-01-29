2f953a71c1de362839410351f2ca0e7db2519a60
Blake Davis <brickanimator@gmail.com>
2026-01-29 14:38:26 +1300

    Refactored CritterEntry.cs to contain a list of details

--- Engine/Province/Critters/CritterEntry.cs
+++ Engine/Province/Critters/CritterEntry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using VictorianAnimalGame.Engine.Defines;
 using VictorianAnimalGame.Engine.Province.Critters.Cultures;
@@ -7,83 +8,35 @@ using VictorianAnimalGame.Engine.Province.Critters.Species;
 namespace VictorianAnimalGame.Engine.Province.Critters
 {
     [StructLayout(LayoutKind.Sequential)]
-    public record struct CritterEntry
+    public readonly record struct CritterEntry(SpeciesType Species, CultureType Culture, CritterClass Class)
     {
-        private readonly short _year;
-        private CritterDetails _critterDetails;
-        public SpeciesType Species { get; }
-        public CultureType Culture { get; }
-        
-        public CritterLifeStage LifeStage => GetCritterAge();
-        
-        public CritterEntry(short newYear, SpeciesType newSpecies, 
-            CultureType newCulture, CritterDetails newDetails)
-        {
-            _year = newYear;
-            Species = newSpecies;
-            Culture = newCulture;
-            _critterDetails = newDetails;
-        }
-        
-        public short GetCritterYear()
-        {
-            return _year;
-        }
+        public SpeciesType Species { get; } = Species;
+        public CritterClass Class { get; } = Class;
+        public CultureType Culture { get; } = Culture;
+        public List<CritterDetails> Details { get; } = [];
 
-        public CritterDetails GetCritterDetails()
+        public void SortDetails()
         {
-            return _critterDetails;
-        }
-        
-        public SpeciesType GetCritterSpecies()
-        {
-            return Species;
-        }
-        
-        public CultureType GetCritterCulture()
-        {
-            return Culture;
-        }
-        
-        public uint GetCritterClassCount(CritterClass critterClass)
-        {
-            return _critterDetails.GetCritterCount(critterClass);
-        }
-        
-        public uint GetCritterTotalCount()
-        {
-            return _critterDetails.GetCritterTotalCount();
-        }
-
-        public CritterLifeStage GetCritterAge()
-        {
-            CritterDefines.Species.TryGetValue(Species, out var species);
-            int age = DateDefines.Year - _year;
-
-            return age switch
-            {
-                _ when age < species.AdolescentAge => CritterLifeStage.Young,
-                _ when age < species.AdultAge => CritterLifeStage.Adolescent,
-                _ when age < species.ElderAge => CritterLifeStage.Adult,
-                _ => CritterLifeStage.Elder
-            };
+            Details.Sort();
         }
         
         public bool Equals(CritterEntry newCritter) =>
-            (_culture: Culture, _species: Species, _year).Equals(
-                (newCritter.Culture, newCritter.Species, newCritter._year));
+            (Culture, Species, Class).Equals(
+                (newCritter.Culture, newCritter.Species, newCritter.Class));
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Culture, Species, _year);
+            return HashCode.Combine(Culture, Species, Class);
         }
 
         public override string ToString()
         {
-            return $"Current {CritterDefines.Species[Species].SpeciesName}: " +
-                   $"{Culture}" +
-                   $"/{_year}" +
-                   $"/{GetHashCode()}/{LifeStage}/{_critterDetails}";
+            string s = $"Current {Class} {Species.Name}: {Culture}/{GetHashCode()}";
+            foreach (var v in Details)
+            {
+                s += $"\n{v}";
+            }
+            return s;
         }
     }
 }
