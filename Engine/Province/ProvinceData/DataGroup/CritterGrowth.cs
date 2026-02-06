7e5157cba184a631730f4ac3aa4cf7e441deac1e
Blake Davis <brickanimator@gmail.com>
2026-02-07 03:01:23 +1300

    Added Index Logic to CritterGrowth.cs

--- Engine/Province/ProvinceData/DataGroup/CritterGrowth.cs
+++ Engine/Province/ProvinceData/DataGroup/CritterGrowth.cs
@@ -14,23 +14,27 @@ public record struct CritterGrowth
     private int _elderIndex;
     private int _length;
     private SpeciesType _species;
-    
 
     private void AdjustIndex(ReadOnlySpan<CritterDetails> critterSpan)
     {
         _adultIndex = critterSpan.BinarySearchByYearValue(DateDefines.Year - _species.AdultAge + 1);
         _elderIndex = critterSpan.BinarySearchByYearValue(DateDefines.Year - _species.ElderAge);
+        _length = critterSpan.Length;
     }
     
-    public CritterGrowth GetFertilityData(ReadOnlySpan<CritterDetails> critterSpan)
+    public void GetFertilityData(ReadOnlySpan<CritterDetails> critterSpan)
     {
+        if (critterSpan.Length != _length)
+        {
+            AdjustIndex(critterSpan);
+        }
+        
         ReadOnlySpan<CritterDetails> fertileRange = critterSpan[_elderIndex.._adultIndex];
         FrozenDictionary<int, float> fertilityValues = _species.BirthsByAge;
-        CritterGrowth critterGrowth = new CritterGrowth();
         foreach (var critter in fertileRange)
         {
             float yearlyValue = fertilityValues[DateDefines.Year - critter.Year];
-            critterGrowth.BirthRate += (int)Math.Round(yearlyValue * (critter.Dependants));
+            BirthRate += (int)Math.Round(yearlyValue * (critter.Dependants));
             // if (critter.Occupied > critter.Dependants)
             // {
             //     critterBirthRate.BirthRate += (int)Math.Round(yearlyValue * (critter.Dependants));
@@ -39,10 +43,7 @@ public record struct CritterGrowth
             // {
             //     critterBirthRate.BirthRate += (int)Math.Round(yearlyValue * (critter.Occupied));
             // }
-
         }
-        //Console.WriteLine($"{_species.Name} {critterBirthRate}");
-        return critterGrowth;
     }
     
     public override string ToString()
