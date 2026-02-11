a80831343150553d09f3e3248abadfb6d59d6774
Blake Davis <brickanimator@gmail.com>
2026-02-11 18:17:19 +1300

    Added new implimentation of CritterDetails to CritterGrowth

--- Engine/Province/ProvinceData/Growth/CritterGrowth.cs
+++ Engine/Province/ProvinceData/Growth/CritterGrowth.cs
@@ -7,33 +7,22 @@ using VictorianAnimalGame.Engine.Extensions;
 
 namespace VictorianAnimalGame.Engine.Province.ProvinceData.Growth;
 
-public class CritterGrowth(SpeciesType newSpecies)
+public class CritterGrowth(SpeciesType Species)
 {
     private double _weeklyValue;
     private int DailyBirths => (int)(_weeklyValue / DateDefines.WeeklyDaysAmount).ProbabilisticRound();
-    //public float EconomicData = 1;
-    private int _adultIndex;
-    private int _elderIndex;
-    private int _length;
 
-    public void CalculateWeeklyBirths(ReadOnlySpan<CritterDetails> critterSpan, float birthRate = 5f)
+    public void CalculateWeeklyBirths(CritterDetails critterDetails, float birthRate = 2f)
     {
-        ReadOnlySpan<CritterDetails> detailsSpan = critterSpan;
+        ReadOnlySpan<ushort> dependantsSpan = critterDetails.Dependants;
+        ReadOnlySpan<ushort> workersSpan = critterDetails.Workers;
         _weeklyValue = 0;
         
-        if (detailsSpan.Length != _length)
+        FrozenDictionary<int, float> fertilityValues = Species.BirthDistribution;
+        foreach (var value in fertilityValues)
         {
-            _adultIndex = detailsSpan.BinarySearchByYearValue(DateDefines.Year - newSpecies.AdultAge + 1);
-            _elderIndex = detailsSpan.BinarySearchByYearValue(DateDefines.Year - newSpecies.ElderAge);
-            _length = detailsSpan.Length;
-        }
-        
-        ReadOnlySpan<CritterDetails> fertileRange = detailsSpan[_elderIndex.._adultIndex];
-        FrozenDictionary<int, float> fertilityValues = newSpecies.BirthDistribution;
-        foreach (var critter in fertileRange)
-        {
-            float weeklyCurve = fertilityValues[DateDefines.Year - critter.Year] * birthRate / DateDefines.WeeksPerYear;
-            _weeklyValue = weeklyCurve * (critter.Total / 2f);
+            float weeklyCurve = value.Value * birthRate / DateDefines.WeeksPerYear;
+            _weeklyValue += weeklyCurve * (dependantsSpan[value.Key] / 2f);
             // if (critter.Occupied > critter.Dependants)
             // {
             //     critterBirthRate.BirthRate += (int)Math.Round(yearlyValue * (critter.Dependants));
@@ -48,6 +37,8 @@ public class CritterGrowth(SpeciesType newSpecies)
     
     public override string ToString()
     {
-        return $"Births: {DailyBirths}/{_weeklyValue.ProbabilisticRound()}/{(_weeklyValue * DateDefines.WeeksPerYear).ProbabilisticRound()}|Index:{_elderIndex}.{_adultIndex}.{_length}";
+        return $"Births: {DailyBirths}/{_weeklyValue.ProbabilisticRound()}/" +
+               $"{(_weeklyValue * DateDefines.WeeksPerYear).ProbabilisticRound()}";
+        //$"|Index:{_elderIndex}.{_adultIndex}.{_length}";
     }
 }
