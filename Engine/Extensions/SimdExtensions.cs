3e7686720a2f90f6f3d201f14174b8707e0b7f9a
Blake Davis <brickanimator@gmail.com>
2026-02-14 22:11:17 +1300

    Created CritterBirths Simd

--- Engine/Extensions/SimdExtensions.cs
+++ Engine/Extensions/SimdExtensions.cs
@@ -83,4 +83,41 @@ public static class SimdExtensions
 
         return (int)totalSum;
     }
+
+    public static float CalculateFertilitySimd(this ushort[] critterArray, SpeciesType Species)
+    {
+        if (critterArray == null || critterArray.Length == 0) return 0;
+
+        Span<ushort> critterSpan = critterArray.AsSpan().Slice(Species.AdultAge, Species.BirthDistribution.Length);
+        
+        ulong totalProbabilityUnits = 0;
+        int i = 0;
+
+        // We process 8 ushorts at a time
+        if (Vector256.IsHardwareAccelerated)
+        {
+            Vector256<ulong> vAcc = Vector256<ulong>.Zero;
+            
+            for (; i < critterSpan.Length; i += 16)
+            {
+                Vector256<ushort> vCountsRaw = Vector256.LoadUnsafe(ref critterSpan[i]);
+                Vector256<ushort> vBirthsRaw = Vector256.LoadUnsafe(ref Species.BirthDistribution[i]);
+                
+                (Vector256<uint> popLow, Vector256<uint> popHigh) = Vector256.Widen(vCountsRaw);
+                (Vector256<uint> rateLow, Vector256<uint> rateHigh) = Vector256.Widen(vBirthsRaw);
+                
+                Vector256<uint> prodLow = Vector256.Multiply(popLow, rateLow);
+                Vector256<uint> prodHigh = Vector256.Multiply(popHigh, rateHigh);
+                
+                vAcc = Vector256.Add(vAcc, Vector256.WidenLower(prodLow));
+                vAcc = Vector256.Add(vAcc, Vector256.WidenUpper(prodLow));
+                vAcc = Vector256.Add(vAcc, Vector256.WidenLower(prodHigh));
+                vAcc = Vector256.Add(vAcc, Vector256.WidenUpper(prodHigh));
+            }
+            
+            totalProbabilityUnits += Vector256.Sum(vAcc);
+        }
+        
+        return totalProbabilityUnits / 65535f;
+    }
 }
