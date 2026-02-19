c5c53914cbf799c938658166e38c2ff87e13f6dc
Blake Davis <brickanimator@gmail.com>
2026-02-20 04:20:03 +1300

    Removed errors inside unimplimented CritterCulture

--- VictorianAnimalGame/Engine/Critters/Cultures/CritterCulture.cs
+++ VictorianAnimalGame/Engine/Critters/Cultures/CritterCulture.cs
@@ -1,9 +1,9 @@
 ﻿namespace VictorianAnimalGame.Engine.Critters.Cultures;
 
-public class CritterCulture
-{
-    private CultureType _cultureId;
-    private string CultureName;
-    private int CultureID;
-    private CulturalValue[] CultureValues;
-}
+// public class CritterCulture
+// {
+//     private CultureType _cultureId;
+//     private string CultureName;
+//     private int CultureID;
+//     private CulturalValue[] CultureValues;
+// }
