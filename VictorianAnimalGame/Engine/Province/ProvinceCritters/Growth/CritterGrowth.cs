c2d54966edc97b524539ab93d0c282ec5d4c26f1
Blake Davis <brickanimator@gmail.com>
2026-02-20 04:19:00 +1300

    Added Birth Logic to CritterGrowth

--- VictorianAnimalGame/Engine/Province/ProvinceCritters/Growth/CritterGrowth.cs
+++ VictorianAnimalGame/Engine/Province/ProvinceCritters/Growth/CritterGrowth.cs
@@ -1,29 +1,42 @@
-﻿using System;
-using System.Collections.Frozen;
-using VictorianAnimalGame.Engine.Critters;
+﻿using VictorianAnimalGame.Engine.Critters;
 using VictorianAnimalGame.Engine.Critters.Species;
 using VictorianAnimalGame.Engine.Defines;
 using VictorianAnimalGame.Engine.Extensions;
+using VictorianAnimalGame.Engine.Randomness;
+using YamlDotNet.Core.Events;
 
 namespace VictorianAnimalGame.Engine.Province.ProvinceCritters.Growth;
 
 public class CritterGrowth(SpeciesType Species)
 {
-    private double _weeklyValue;
-    private int DailyBirths => (int)(_weeklyValue / DateDefines.WeeklyDaysAmount).ProbabilisticRound();
+    private float _weeklyValue;
+    private float _dailyValue;
 
-    public void CalculateWeeklyBirths(CritterDetails critterDetails, float birthRate = 2.5f)
+    public void DailyProcessing(CritterDetails critterDetails, ScalarRng birthRng)
+    {
+        critterDetails.AddBirths((ushort)(_dailyValue + birthRng.NextFloat()));
+    }
+    
+    public void WeeklyProcessing(CritterDetails critterDetails) 
+    {
+        // TODO Have a birthrate modifier passed in
+        SetWeeklyBirths(critterDetails);
+    }
+
+    private void SetWeeklyBirths(CritterDetails critterDetails, float birthRate = 2.5f)
     {
         _weeklyValue = 0;
         _weeklyValue += critterDetails.Dependants.CalculateFertilitySimd(Species);
         _weeklyValue += critterDetails.Workers.CalculateFertilitySimd(Species);
         _weeklyValue = _weeklyValue * birthRate / DateDefines.WeeksPerYear;
+        _dailyValue = _weeklyValue / DateDefines.WeeklyDaysAmount;
     }
     
     public override string ToString()
     {
-        return $"Births: {DailyBirths}/{_weeklyValue.ProbabilisticRound()}/" +
-               $"{(_weeklyValue * DateDefines.WeeksPerYear).ProbabilisticRound()}";
-        //$"|Index:{_elderIndex}.{_adultIndex}.{_length}";
+        return $"Births: {_dailyValue}/{_weeklyValue}/" +
+               $"{_weeklyValue * DateDefines.WeeksPerYear}";
     }
+
+
 }
