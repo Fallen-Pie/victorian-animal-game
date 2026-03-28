d861bbf542d616f130456ac4f4e68806a08c0d5a
Blake Davis <brickanimator@gmail.com>
2026-03-28 22:16:27 +1300

    Added AdminProperty

---
+++ VictorianAnimalGame/Engine/Components/Property/PropertyTypes/AdminProperty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VictorianAnimalGame.Engine.Components.Property.PropertyCategories;
+
+namespace VictorianAnimalGame.Engine.Components.Property.PropertyTypes;
+
+public class AdminProperty : BaseProperty
+{
+    protected override PropertyId RawId { get; } = new(PropertyCode.Admin);
+
+    public int EmployeeCapacity { get; set; }
+
+    public void ProcessPaperwork() => Console.WriteLine("Admin work in progress...");
+    
+    public override string ToString() => $"{PropertyCode.Admin}|Name: {Name}|Id: {RawId}";
+}
