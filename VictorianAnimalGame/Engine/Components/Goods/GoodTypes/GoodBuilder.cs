9c24fa5a5dbe59fb33608b154521645f129b4c69
Blake Davis <brickanimator@gmail.com>
2026-03-31 19:38:30 +1300

    Redid GoodBuilder to build GoodDetails instead of GoodTypes

--- VictorianAnimalGame/Engine/Components/Goods/GoodTypes/GoodBuilder.cs
+++ VictorianAnimalGame/Engine/Components/Goods/GoodTypes/GoodBuilder.cs
@@ -1,31 +1,31 @@
 ﻿namespace VictorianAnimalGame.Engine.Components.Goods.GoodTypes;
 
-public class GoodTypeBuilder
+public class GoodBuilder
 {
     private string _goodName;
     private float _price;
     private float _bulk;
     
-    public GoodTypeBuilder SetGoodName(string name)
+    public GoodBuilder SetGoodName(string name)
     {
         _goodName = name;
         return this;
     }
     
-    public GoodTypeBuilder SetGoodPrice(float price)
+    public GoodBuilder SetGoodPrice(float price)
     {
         _price = price;
         return this;
     }
     
-    public GoodTypeBuilder SetGoodBulk(float bulk)
+    public GoodBuilder SetGoodBulk(float bulk)
     {
         _bulk = bulk;
         return this;
     }
     
-    public GoodType Build()
+    public GoodDetails Build()
     {
-        return new GoodType(_goodName, _price, _bulk);
+        return new GoodDetails(_goodName, _price, _bulk);
     }
 }
