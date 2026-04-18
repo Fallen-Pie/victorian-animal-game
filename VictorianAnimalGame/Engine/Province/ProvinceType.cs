e990f846bd32b452b8f4afa9ba0ba6b0bc5b69ba
Blake Davis <brickanimator@gmail.com>
2026-04-19 03:01:05 +1200

    Added ProvinceId

---
+++ VictorianAnimalGame/Engine/Province/ProvinceType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VictorianAnimalGame.Engine.Province;
+
+public readonly struct ProvinceType(ushort provinceId) : IEquatable<ProvinceType>
+{
+    private ushort ProvinceId { get; } = provinceId;
+    
+    public override bool Equals(object obj) => obj is ProvinceType other && Equals(other);
+    public bool Equals(ProvinceType other) => ProvinceId == other.ProvinceId;
+    public override int GetHashCode() => ProvinceId.GetHashCode();
+    public static bool operator ==(ProvinceType left, ProvinceType right) => left.Equals(right);
+    public static bool operator !=(ProvinceType left, ProvinceType right) => !(left == right);
+    public override string ToString() => ProvinceId.ToString();
+}
