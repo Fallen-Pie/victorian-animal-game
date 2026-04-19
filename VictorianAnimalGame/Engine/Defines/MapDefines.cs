a6a410b027bdb579e2502729b93448d629f24217
Blake Davis <brickanimator@gmail.com>
2026-04-20 03:43:59 +1200

    Changed MapDefines to use the new Extension method for Colour

--- VictorianAnimalGame/Engine/Defines/MapDefines.cs
+++ VictorianAnimalGame/Engine/Defines/MapDefines.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Frozen;
 using System.Collections.Generic;
 using Godot;
+using VictorianAnimalGame.Engine.Extensions;
 using VictorianAnimalGame.Engine.Province;
 using VictorianAnimalGame.FileReader.MapReader;
-using VictorianAnimalGame.FileReader.MapReader.Strategies;
 
 namespace VictorianAnimalGame.Engine.Defines;
 
@@ -17,28 +17,26 @@ public static class MapDefines
         //TODO Read these from files
         return new Dictionary<uint, ProvinceType>
         {
-            // Strip alpha from your definition colors so they match the scanner logic
-            { MapScanner.ColorToMapUint(new Color("FF7F27")) & 0x00FFFFFF, new ProvinceType(1) }, // Orange
-            { MapScanner.ColorToMapUint(new Color("ED1C24")) & 0x00FFFFFF, new ProvinceType(2) }, // Red
-            { MapScanner.ColorToMapUint(new Color("880015")) & 0x00FFFFFF, new ProvinceType(3) }, // Dark Red
+            { new Color("FF7F27").ToUint(), new ProvinceType(1) }, // Orange
+            { new Color("ED1C24").ToUint(), new ProvinceType(2) }, // Red
+            { new Color("880015").ToUint(), new ProvinceType(3) }, // Dark Red
             
-            { MapScanner.ColorToMapUint(new Color("22B14C")) & 0x00FFFFFF, new ProvinceType(4) }, // Light Green
-            { MapScanner.ColorToMapUint(new Color("1FA046")) & 0x00FFFFFF, new ProvinceType(5) }, // Green
-            { MapScanner.ColorToMapUint(new Color("B5E61D")) & 0x00FFFFFF, new ProvinceType(6) }, // Yellow Green
-            { MapScanner.ColorToMapUint(new Color("008000")) & 0x00FFFFFF, new ProvinceType(7) }, // Forest Green
-            { MapScanner.ColorToMapUint(new Color("006400")) & 0x00FFFFFF, new ProvinceType(8) }, // Dark Green
+            { new Color("22B14C").ToUint(), new ProvinceType(4) }, // Light Green
+            { new Color("1FA046").ToUint(), new ProvinceType(5) }, // Green
+            { new Color("B5E61D").ToUint(), new ProvinceType(6) }, // Yellow Green
+            { new Color("008000").ToUint(), new ProvinceType(7) }, // Forest Green
+            { new Color("006400").ToUint(), new ProvinceType(8) }, // Dark Green
         }.ToFrozenDictionary();
     }
     
     private static ProvinceType[] DefineMapData()
     {
         Image provincesImage = Image.LoadFromFile("Data/Map/provinces.bmp");
-        //Image riverImage = Image.LoadFromFile("res://maps/rivers.png");
-
-
-        // 3. Run the Province Strategy
+        
+        //TODO Make this Reusable
         return MapScanner.Scan(provincesImage);
 
+        //Image riverImage = Image.LoadFromFile("res://maps/rivers.png");
         // 4. Run the River Strategy, passing in the province data
         //var riverStrategy = new RiverDetectionStrategy(new Color("0000FF"), provinceMapData); // Blue rivers
         //HashSet<int> riverProvinces = MapScanner.Scan(riverImage, riverStrategy);
