c8bf833582df026b644051008aa381b3491ac759
Blake Davis <brickanimator@gmail.com>
2026-04-20 03:43:31 +1200

    Added extension for Color to return Uint rather than color object

---
+++ VictorianAnimalGame/Engine/Extensions/ColourExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace VictorianAnimalGame.Engine.Extensions;
+
+public static class ColourExtensions
+{
+    public static uint ToUint(this Color color)
+    {
+        return ((uint)color.A8 << 24) | ((uint)color.B8 << 16) | ((uint)color.G8 << 8) | (uint)color.R8;
+    }
+}
