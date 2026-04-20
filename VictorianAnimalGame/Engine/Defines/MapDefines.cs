305b09c876a81cda58af6acb1de5f7f559405b64
Blake Davis <brickanimator@gmail.com>
2026-04-21 05:56:30 +1200

    Updated MapDefines to read Province Yaml files

--- VictorianAnimalGame/Engine/Defines/MapDefines.cs
+++ VictorianAnimalGame/Engine/Defines/MapDefines.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Frozen;
+﻿using System;
+using System.Collections.Frozen;
 using System.Collections.Generic;
 using Godot;
 using VictorianAnimalGame.Engine.Extensions;
 using VictorianAnimalGame.Engine.Province;
+using VictorianAnimalGame.Engine.Province.Builder;
+using VictorianAnimalGame.Engine.Province.Types;
 using VictorianAnimalGame.FileReader.DataReader;
+using VictorianAnimalGame.FileReader.DataReader.DataConfig;
 
 namespace VictorianAnimalGame.Engine.Defines;
 
@@ -14,19 +18,67 @@ public static class MapDefines
 
     private static FrozenDictionary<uint, ProvinceId> DefineProvinces()
     {
-        //TODO Read these from files
-        return new Dictionary<uint, ProvinceId>
+        List<ProvinceConfig> data = YamlReader.ReadFiles<ProvinceConfig>("Data/Map/Provinces/");
+        Dictionary<uint, ProvinceId> h = [];
+        
+        foreach (var province in data)
         {
-            { new Color("FF7F27").ToUint(), new ProvinceId(1) }, // Orange
-            { new Color("ED1C24").ToUint(), new ProvinceId(2) }, // Red
-            { new Color("880015").ToUint(), new ProvinceId(3) }, // Dark Red
+            var provinceData = province.Province;
+            IProvinceBuilder builder;
             
-            { new Color("22B14C").ToUint(), new ProvinceId(4) }, // Light Green
-            { new Color("1FA046").ToUint(), new ProvinceId(5) }, // Green
-            { new Color("B5E61D").ToUint(), new ProvinceId(6) }, // Yellow Green
-            { new Color("008000").ToUint(), new ProvinceId(7) }, // Forest Green
-            { new Color("006400").ToUint(), new ProvinceId(8) }, // Dark Green
-        }.ToFrozenDictionary();
+            switch (provinceData.Type)
+            {
+                case "Land":
+                    builder = new LandProvinceBuilder();
+                    break;
+                case "Sea":
+                    builder = new SeaProvinceBuilder();
+                    break;
+                case "Wasteland":
+                    builder = new WastelandProvinceBuilder();
+                    break;
+                default:
+                    throw new ArgumentException("Unknown province type: " + provinceData.Type);
+            }
+            
+            IProvince newProvince = builder.SetColour(new Color(provinceData.Colour).ToUint())
+                .SetName(provinceData.Name)
+                .SetProvinceId(new ProvinceId(provinceData.Id))
+                .Build();
+            
+            h.Add(new Color(provinceData.Colour).ToUint(), new ProvinceId(provinceData.Id));
+            
+            // SpeciesType newSpeciesType = new SpeciesType(i);
+            // speciesTypes.Add(speciesData.Name, newSpeciesType);
+            //
+            // SpeciesDetails newSpeciesDetails = new SpeciesBuilder()
+            //     .SetSpeciesName(speciesData.Name)
+            //     .SetSpeciesType(newSpeciesType)
+            //     .SetAges(speciesData.Ages)
+            //     .SetPeakFertility(speciesData.PeakFertility)
+            //     .SetFoodConsumption(speciesData.FoodConsumption)
+            //     .SetWorkforceValue(speciesData.WorkforceValue)
+            //     .Build();
+            // species.Add(newSpeciesType, newSpeciesDetails);
+            //
+            // Console.WriteLine(newSpeciesDetails);
+        }
+        
+        //TODO Read these from files
+        // return new Dictionary<uint, ProvinceId>
+        // {
+        //     { new Color("FF7F27").ToUint(), new ProvinceId(1) }, // Orange
+        //     { new Color("ED1C24").ToUint(), new ProvinceId(2) }, // Red
+        //     { new Color("880015").ToUint(), new ProvinceId(3) }, // Dark Red
+        //     
+        //     { new Color("22B14C").ToUint(), new ProvinceId(4) }, // Light Green
+        //     { new Color("1FA046").ToUint(), new ProvinceId(5) }, // Green
+        //     { new Color("B5E61D").ToUint(), new ProvinceId(6) }, // Yellow Green
+        //     { new Color("008000").ToUint(), new ProvinceId(7) }, // Forest Green
+        //     { new Color("006400").ToUint(), new ProvinceId(8) }, // Dark Green
+        // }.ToFrozenDictionary();
+
+        return h.ToFrozenDictionary();
     }
     
     private static ProvinceId[] DefineMapData()
