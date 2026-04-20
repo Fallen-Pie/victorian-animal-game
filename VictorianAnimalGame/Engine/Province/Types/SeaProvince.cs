22bb00cf31aedd8332a8bc20ead8ff07b22965b4
Blake Davis <brickanimator@gmail.com>
2026-04-21 05:39:22 +1200

    changed SeaProvince to use new IProvince Interface

--- VictorianAnimalGame/Engine/Province/Types/SeaProvince.cs
+++ VictorianAnimalGame/Engine/Province/Types/SeaProvince.cs
@@ -4,6 +4,11 @@ namespace VictorianAnimalGame.Engine.Province.Types;
 
 public class SeaProvince : IProvince
 {
+    public SeaProvince(ProvinceId newId, uint newMapColour, string newName) : base(newId, newMapColour, newName)
+    {
+        
+    }
+
     public string GetDetails()
     {
         throw new NotImplementedException();
