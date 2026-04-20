f33149a8e2c457fadcb810ca5e6ab2860b862994
Blake Davis <brickanimator@gmail.com>
2026-04-21 05:39:31 +1200

    Added WastelandProvince.cs

---
+++ VictorianAnimalGame/Engine/Province/Types/WastelandProvince.cs
@@ -0,0 +1,8 @@
+﻿namespace VictorianAnimalGame.Engine.Province.Types;
+
+public class WastelandProvince : IProvince
+{
+    public WastelandProvince(ProvinceId newId, uint newMapColour, string newName) : base(newId, newMapColour, newName)
+    {
+    }
+}
