56e0b94c201400dc67f9969e3cc6dcd4eac46339
Blake Davis <brickanimator@gmail.com>
2026-04-21 05:38:30 +1200

    Created a ProvinceBuilder

---
+++ VictorianAnimalGame/Engine/Province/Builder/IProvinceBuilder.cs
@@ -0,0 +1,54 @@
+﻿using VictorianAnimalGame.Engine.Province.Types;
+
+namespace VictorianAnimalGame.Engine.Province.Builder;
+
+public abstract class IProvinceBuilder
+{
+    protected ProvinceId provinceId;
+    protected uint mapColour;
+    protected string name;
+
+    public IProvinceBuilder SetProvinceId(ProvinceId newProvinceId)
+    {
+        provinceId = newProvinceId;
+        return this;
+    }
+    
+    public IProvinceBuilder SetColour(uint newMapColour)
+    {
+        mapColour = newMapColour;
+        return this;
+    }
+    
+    public IProvinceBuilder SetName(string newMame)
+    {
+        name = newMame;
+        return this;
+    }
+    
+    public abstract IProvince Build();
+}
+
+public class LandProvinceBuilder : IProvinceBuilder
+{
+    public override LandProvince Build()
+    {
+        return new LandProvince(provinceId, mapColour, name);
+    }
+}
+
+public class SeaProvinceBuilder : IProvinceBuilder
+{
+    public override SeaProvince Build()
+    {
+        return new SeaProvince(provinceId, mapColour, name);
+    }
+}
+
+public class WastelandProvinceBuilder : IProvinceBuilder
+{
+    public override WastelandProvince Build()
+    {
+        return new WastelandProvince(provinceId, mapColour, name);
+    }
+}
