62595fd21559c1992a8ea73fafab6edf4c00081a
Blake Davis <brickanimator@gmail.com>
2026-04-22 05:26:11 +1200

    Created ProvinceBuilderDetails.cs

---
+++ VictorianAnimalGame/Engine/Province/Builder/ProvinceBuilderDetails.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace VictorianAnimalGame.Engine.Province.Builder;
+
+public class ProvinceBuilderDetails
+{
+    public uint PixelSize;
+    public Vector2I Centre;
+    public Dictionary<ProvinceId, uint> ProvinceNeighbours = [];
+    
+    long sumXPixels;
+    long sumYPixels;
+
+    public void AddPixel(int x, int y)
+    {
+        PixelSize++;
+        sumXPixels += x;
+        sumYPixels += y;
+    }
+    
+    public void GetCentre()
+    {
+        long centroidX = sumXPixels / PixelSize;
+        long centroidY = sumYPixels / PixelSize;
+        Centre = new Vector2I((int)centroidX, (int)centroidY);
+    }
+    
+    public void AddNeighbours(ProvinceId neighbour)
+    {
+        ProvinceNeighbours.TryAdd(neighbour, 0);
+    }
+    
+    public void GetNeighboursDistance(ref Dictionary<ProvinceId, Vector2I> provinceCentres)
+    {
+        foreach (var province in ProvinceNeighbours.Keys)
+        {
+            uint distance = (uint)Centre.DistanceTo(provinceCentres[province]);
+            ProvinceNeighbours[province] = distance;
+        }
+    }
+
+    public void Deconstruct(out uint size, out Dictionary<ProvinceId, uint> neighbours, out Vector2I centre)
+    {
+        size = PixelSize;
+        neighbours = ProvinceNeighbours;
+        centre = Centre;
+    }
+    
+
+}
