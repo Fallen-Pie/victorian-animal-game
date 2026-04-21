b4a55981188d00b59f4a2adb3ac843e1b457168f
Blake Davis <brickanimator@gmail.com>
2026-04-22 05:27:56 +1200

    Added Size, Neighbours and Centre to IProvince.cs

--- VictorianAnimalGame/Engine/Province/Types/IProvince.cs
+++ VictorianAnimalGame/Engine/Province/Types/IProvince.cs
@@ -1,4 +1,6 @@
-using System;
+using System.Collections.Frozen;
+using Godot;
+using VictorianAnimalGame.Engine.Province.Builder;
 
 namespace VictorianAnimalGame.Engine.Province.Types;
 
@@ -6,20 +8,36 @@ public abstract class IProvince
 {
     public readonly ProvinceId Id;
     public readonly uint MapColour;
+
+    public readonly uint Size;
+    public readonly FrozenDictionary<ProvinceId, uint> Neighbours;
+    public readonly Vector2I Centre;
+    
     public string Name;
 
-    public uint Size;
-    public uint Neighbours;
-
-    protected IProvince(ProvinceId newId, uint newMapColour, string newName)
+    protected IProvince(ProvinceId newId, uint newMapColour, string newName, ProvinceBuilderDetails details)
     {
         Id = newId;
         MapColour = newMapColour;
         Name = newName;
+
+        var (pixelSize, provinceNeighbours, provinceCentre) = details;
+        Size = pixelSize;
+        Neighbours = provinceNeighbours.ToFrozenDictionary();
+        Centre = provinceCentre;
+    }
+
+    public string GetSimpleProvince()
+    {
+        return $"{GetType().Name[..^8]}:{Name}|{Id}";
     }
 
     public override string ToString()
     {
-        return $"{GetType().Name}: {Name}|{Id}|{MapColour:X}|Size {Size}|Neighbours {Neighbours}";
+        string s = $"{GetType().Name[..^8]}:{Name}|{Id}|{MapColour:X}|Size {Size}|Neighbours:";
+        foreach (var neighbour in Neighbours)
+               s += neighbour.Key.Province.GetSimpleProvince() + "/Dist:" + neighbour.Value + "|";
+        s += $"Centre {Centre}";
+        return s;
     }
 }
