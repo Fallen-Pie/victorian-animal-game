4ae7d763d9f7fba04ea5974bd32033554988ab48
Blake Davis <brickanimator@gmail.com>
2026-04-28 03:10:46 +1200

    Created 3 province types which are then inherited

---
+++ VictorianAnimalGame/Engine/Province/Types/Sea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace VictorianAnimalGame.Engine.Province.Types;
+
+public abstract class Sea(
+    ProvinceId newId, 
+    uint newMapColour, 
+    string newName, 
+    uint pixelSize, 
+    Dictionary<ProvinceId, uint> provinceNeighbours, 
+    Vector2I provinceCentre)
+    : IProvince(newId, newMapColour, newName, pixelSize, provinceNeighbours, provinceCentre)
+{
+    
+}
