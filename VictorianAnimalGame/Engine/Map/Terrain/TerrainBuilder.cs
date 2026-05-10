0c761634d0c4e920e05f75de1b606a1af163f4a1
Blake Davis <brickanimator@gmail.com>
2026-05-11 03:36:34 +1200

    Created TypographyBuilder

---
+++ VictorianAnimalGame/Engine/Map/Terrain/TerrainBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace VictorianAnimalGame.Engine.Map.Terrain;
+
+public class TerrainBuilder
+{
+    
+}
+
+public class TypographyBuilder
+{
+    private string _name;
+    private string _colour;
+    private TypographyType _type;
+    
+    public TypographyBuilder SetTypographyName(string name)
+    {
+        _name = name;
+        return this;
+    }
+    
+    public TypographyBuilder SetTypographyColour(string colour)
+    {
+        _colour = colour;
+        return this;
+    }
+    
+    public TypographyBuilder SetTypographyType(TypographyType type)
+    {
+        _type = type;
+        return this;
+    }
+    
+    public TypographyDetails Build()
+    {
+        return new TypographyDetails(_name, _colour, _type);
+    }
+}
