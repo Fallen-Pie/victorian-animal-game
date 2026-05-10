b0289744c70b1bd51a212d19c6ccf74ead298d68
Blake Davis <brickanimator@gmail.com>
2026-05-11 03:36:21 +1200

    Created TypographyType and Details

---
+++ VictorianAnimalGame/Engine/Map/Terrain/TypographyType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VictorianAnimalGame.Engine.Map.Terrain;
+
+public readonly struct TypographyType(byte typographyId) : IEquatable<TypographyType>
+{
+    private ushort TypographyId { get; } = typographyId;
+    
+    
+    
+    public override bool Equals(object obj) => obj is TypographyType other && Equals(other);
+    public bool Equals(TypographyType other) => TypographyId == other.TypographyId;
+    public override int GetHashCode() => TypographyId.GetHashCode();
+    public static bool operator ==(TypographyType left, TypographyType right) => left.Equals(right);
+    public static bool operator !=(TypographyType left, TypographyType right) => !(left == right);
+    public override string ToString() => typographyId.ToString();
+}
+
+public readonly record struct TypographyDetails(string newName, string newMapColour, TypographyType newType)
+{
+    public readonly string Name = newName;
+    public readonly string Colour = newMapColour;
+    public readonly TypographyType Type = newType;
+
+    public override string ToString() => $"Name:{Name}|Colour:#{Colour}|Type:{Type}";
+}
