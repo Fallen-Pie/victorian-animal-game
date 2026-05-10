5461d46ed9ffc3e4aa1143ffeda7dd3d5e08d40c
Blake Davis <brickanimator@gmail.com>
2026-05-11 03:35:43 +1200

    Added Typography config to read Yaml Typography file

---
+++ VictorianAnimalGame/FileReader/DataReader/DataConfig/TypographyConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace VictorianAnimalGame.FileReader.DataReader.DataConfig;
+
+public class TypographyConfig : IDataConfig
+{
+    public TypographyData Typography { get; set; }
+}
+
+public class TypographyData
+{
+    public string Name { get; set; }
+    
+    public string Colour { get; set; }
+}
