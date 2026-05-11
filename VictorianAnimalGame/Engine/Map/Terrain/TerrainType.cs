950c9de78096dbccedf3fd5f1f8f6da5ea00cc3c
Blake Davis <brickanimator@gmail.com>
2026-05-12 03:39:23 +1200

    Created TerrainType struct

---
+++ VictorianAnimalGame/Engine/Map/Terrain/TerrainType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VictorianAnimalGame.Engine.Map.Terrain;
+
+public readonly record struct TerrainType(TypographyType newTypography, VegetationType newVegetation)
+{
+    private readonly TypographyType _typographyData = newTypography;
+    private readonly VegetationType _vegetationData = newVegetation;
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_typographyData, _vegetationData);
+    }
+
+    public override string ToString()
+    {
+        return $"{_typographyData.Name}-{_vegetationData.Name}";
+    }
+}
