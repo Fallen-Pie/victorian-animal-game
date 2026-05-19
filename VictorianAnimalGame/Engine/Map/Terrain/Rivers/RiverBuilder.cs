69c96bee7375ee6d134097c3cf5c0d22b4c26d95
Blake Davis <brickanimator@gmail.com>
2026-05-20 02:51:38 +1200

    Added more details to RiverBuilder and way to create a RiverDetails

--- VictorianAnimalGame/Engine/Map/Terrain/Rivers/RiverBuilder.cs
+++ VictorianAnimalGame/Engine/Map/Terrain/Rivers/RiverBuilder.cs
@@ -1,23 +1,28 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using Godot;
 using VictorianAnimalGame.Engine.Map.Terrain.Typography;
 using VictorianAnimalGame.Engine.Province;
 
 namespace VictorianAnimalGame.Engine.Map.Terrain.Rivers;
 
-public class RiverBuilder(string name, Vector2I startPoint)
+public class RiverBuilder(string name, Vector2I startPoint, ProvinceId startProvince)
 {
     public string name = name;
+    public TypographyType type;
     public Vector2I startPoint = startPoint;
+    public ProvinceId startProvince = startProvince;
     
+    public ProvinceId endProvince;
     public Vector2I endPoint;
     public uint length = 1;
 
     public RiverDirection CurrentBehind = RiverDirection.None;
-    public uint CurrentPositionData;
 
-    public TypographyType startRiver;
-    public TypographyType endRiver;
-    public ProvinceId endProvince;
+    public TypographyType? startRiver;
+    public TypographyType? endRiver;
+
+    public List<Vector2I> deltaEndpoints = [];
+    public bool riverEndpoint = false;
     
     public void Step(RiverDirection newDirection)
     {
@@ -25,8 +30,37 @@ public class RiverBuilder(string name, Vector2I startPoint)
         length++;
     }
 
+    public RiverBuilder SetEndPoint(Vector2I newEndPoint)
+    {
+        endPoint = newEndPoint;
+        return this;
+    }
+    
+    public RiverBuilder SetEndProvince(ProvinceId newEndProvince)
+    {
+        endProvince = newEndProvince;
+        return this;
+    }
+    
+    public void AddDeltaEndpoint(Vector2I newEndpoint)
+    {
+        deltaEndpoints.Add(newEndpoint);
+    }
+    
+    public void SetEndRiver(TypographyType newEndRiver)
+    {
+        endRiver = newEndRiver;
+    }
+
     public RiverDetails Build()
     {
-        return new RiverDetails(name, startPoint, endPoint, length);
+        return new RiverDetails(name, type, startPoint, endPoint, length, 
+            startProvince, endProvince, deltaEndpoints, startRiver, endRiver);
+    }
+
+    public RiverBuilder AddType(TypographyType newType)
+    {
+        type = newType;
+        return this;
     }
 }
