b5ac41715745da5d2cb80630819119eecc206edd
Blake Davis <brickanimator@gmail.com>
2026-05-20 17:42:23 +1200

    Created RegionDetails

---
+++ VictorianAnimalGame/Engine/Map/Regions/RegionDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using VictorianAnimalGame.Engine.Province;
+
+namespace VictorianAnimalGame.Engine.Map.Regions;
+
+public class RegionDetails
+{
+    public string Name { get; }
+    public string Colour { get; }
+    public ProvinceId[] ProvinceIds { get; }
+    
+    public RegionDetails(string newName, string newColour, List<ProvinceId> newProvinces)
+    {
+        Name = newName;
+        Colour = newColour;
+        ProvinceIds = newProvinces.ToArray();
+    }
+}
