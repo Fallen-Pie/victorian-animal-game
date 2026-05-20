43cb31d2401c29259796ed3f9a57f9e77f222bbc
Blake Davis <brickanimator@gmail.com>
2026-05-20 17:42:14 +1200

    Created RegionId

---
+++ VictorianAnimalGame/Engine/Map/Regions/RegionId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VictorianAnimalGame.Engine.Map.Regions;
+
+public readonly record struct RegionId(ushort regionId)
+{
+    private ushort Id { get; } = regionId;
+    
+    public bool Equals(RegionId other) => Id == other.Id;
+    public override int GetHashCode() => Id.GetHashCode();
+    public override string ToString() => Id.ToString();
+}
