5ac89b871381c5d6c0a37c44f72415e47d27c470
Blake Davis <brickanimator@gmail.com>
2026-05-20 17:42:33 +1200

    Created RegionBuilder

---
+++ VictorianAnimalGame/Engine/Map/Regions/RegionBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using VictorianAnimalGame.Engine.Province;
+
+namespace VictorianAnimalGame.Engine.Map.Regions;
+
+public class RegionBuilder
+{
+    private string _name;
+    private string _colour;
+    private List<ProvinceId> _provinces;
+    
+    public RegionBuilder SetRegionName(string name)
+    {
+        _name = name;
+        return this;
+    }
+    
+    public RegionBuilder SetRegionColour(string colour)
+    {
+        _colour = colour;
+        return this;
+    }
+    
+    public RegionBuilder SetRegionProvinces(List<int> provinces)
+    {
+        foreach (ushort provinceId in provinces)
+        {
+            _provinces.Add(new ProvinceId(provinceId));
+        }
+        return this;
+    }
+    
+    public RegionDetails Build()
+    {
+        return new RegionDetails(_name, _colour, _provinces);
+    }
+}
