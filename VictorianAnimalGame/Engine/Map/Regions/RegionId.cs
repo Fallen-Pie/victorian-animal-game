2fcba2f08a05c5818301bf75e2aea89a7f7739ff
Blake Davis <brickanimator@gmail.com>
2026-05-21 22:03:41 +1200

    Added Properties for RegionId to get Name, Colour and containing Provinces

--- VictorianAnimalGame/Engine/Map/Regions/RegionId.cs
+++ VictorianAnimalGame/Engine/Map/Regions/RegionId.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using VictorianAnimalGame.Engine.Defines;
+using VictorianAnimalGame.Engine.Province;
 
 namespace VictorianAnimalGame.Engine.Map.Regions;
 
 public readonly record struct RegionId(ushort regionId)
 {
     private ushort Id { get; } = regionId;
+    public string Name => AnnalsDefines.RegionData[this].Name;
+    public string Colour => AnnalsDefines.RegionData[this].Colour;
+    public ProvinceId[] ProvinceIds => AnnalsDefines.RegionData[this].ProvinceIds;
+
     
     public bool Equals(RegionId other) => Id == other.Id;
     public override int GetHashCode() => Id.GetHashCode();
