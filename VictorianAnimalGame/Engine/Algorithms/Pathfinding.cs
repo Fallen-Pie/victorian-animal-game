7f61bccc5858b933a8a93cede008750349ae79eb
Blake Davis <brickanimator@gmail.com>
2026-05-21 22:02:49 +1200

    Created a Algorithm for getting distance inside a Market Canton (Region)

---
+++ VictorianAnimalGame/Engine/Algorithms/Pathfinding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VictorianAnimalGame.Engine.Map.Regions;
+using VictorianAnimalGame.Engine.Province;
+
+namespace VictorianAnimalGame.Engine.Algorithms;
+
+public static class Pathfinding
+{
+    public static Dictionary<ProvinceId, uint> RegionGetShortestPath(
+        RegionId region, ProvinceId startProvince)
+    {
+        Dictionary<ProvinceId, uint> distances = [];
+        PriorityQueue<ProvinceId, uint> priorityQueue = new PriorityQueue<ProvinceId, uint>();
+
+        foreach (ProvinceId province in region.ProvinceIds)
+        {
+            distances[province] = uint.MaxValue;
+        }
+        distances[startProvince] = 0;
+        priorityQueue.Enqueue(startProvince, 0);
+
+        while (priorityQueue.Count > 0)
+        {
+            priorityQueue.TryDequeue(out ProvinceId current, out uint currentDist);
+
+            if (currentDist > distances[current]) continue;
+            if (!region.ProvinceIds.Contains(current)) continue;
+            
+            foreach (var (edgeId, edgeDistance) in current.Neighbours)
+            {
+                if (!distances.ContainsKey(edgeId)) continue;
+                
+                uint newDist = distances[current] + edgeDistance;
+                if (newDist < distances[edgeId])
+                {
+                    distances[edgeId] = newDist;
+                    priorityQueue.Enqueue(edgeId, newDist);
+                }
+            }
+        }
+        return distances;
+    }
+}
