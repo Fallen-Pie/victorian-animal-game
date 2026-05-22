cdafead928c256cf224402697ef31bc589667e59
Blake Davis <brickanimator@gmail.com>
2026-05-22 18:35:15 +1200

    Created MarketRegion from MarketCantons (might redo this)

---
+++ VictorianAnimalGame/Engine/Components/Markets/Types/MarketRegion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using VictorianAnimalGame.Engine.Algorithms;
+using VictorianAnimalGame.Engine.Map.Cantons;
+using VictorianAnimalGame.Engine.Province;
+
+namespace VictorianAnimalGame.Engine.Components.Markets.Types;
+
+public class MarketRegion : MarketCanton
+{
+    public Dictionary<MarketCanton, uint> MarketCantons { get; }
+    
+    public MarketRegion(CantonId newCanton, ProvinceId newCentre, List<MarketCanton> marketCantons) : base(newCanton, newCentre)
+    {
+        Dictionary<MarketCanton, uint> cantonDistance = [];
+        foreach (MarketCanton canton in marketCantons)
+            cantonDistance[canton] = Pathfinding.GetLandDistance(CentreProvince, canton.CentreProvince);
+        MarketCantons = cantonDistance;
+    }
+
+    
+}
