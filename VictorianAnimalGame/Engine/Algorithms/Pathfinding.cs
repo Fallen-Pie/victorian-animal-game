f798e9098135bb30962bbcf9afbfc6819de16fd4
Blake Davis <brickanimator@gmail.com>
2026-05-22 13:39:32 +1200

    Added A* Pathfinding for distance between two Provinces

--- VictorianAnimalGame/Engine/Algorithms/Pathfinding.cs
+++ VictorianAnimalGame/Engine/Algorithms/Pathfinding.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using VictorianAnimalGame.Engine.Map.Regions;
 using VictorianAnimalGame.Engine.Province;
+using VictorianAnimalGame.Engine.Province.Types;
 
 namespace VictorianAnimalGame.Engine.Algorithms;
 
@@ -27,7 +28,7 @@ public static class Pathfinding
             if (currentDist > distances[current]) continue;
             if (!region.ProvinceIds.Contains(current)) continue;
             
-            foreach (var (edgeId, edgeDistance) in current.Neighbours)
+            foreach ((ProvinceId edgeId, uint edgeDistance) in current.Neighbours)
             {
                 if (!distances.ContainsKey(edgeId)) continue;
                 
@@ -40,5 +41,54 @@ public static class Pathfinding
             }
         }
         return distances;
+    }
+
+    public static List<ProvinceId> GetPathLand(
+        ProvinceId startProvince, ProvinceId endProvince)
+    {
+        PriorityQueue<ProvinceId, uint> openSet = new PriorityQueue<ProvinceId, uint>();
+        var gScore = new Dictionary<ProvinceId, uint>();
+        var cameFrom = new Dictionary<ProvinceId, ProvinceId>();
+        
+        gScore[startProvince] = 0;
+        uint initialFScore = CalculateEuclideanHeuristic(startProvince, endProvince);
+        openSet.Enqueue(startProvince, initialFScore);
+
+        while (openSet.Count > 0)
+        {
+            ProvinceId currentProvince = openSet.Dequeue();
+
+            if (currentProvince == endProvince)
+            {
+                var path = new List<ProvinceId> { currentProvince };
+                while (cameFrom.ContainsKey(currentProvince))
+                {
+                    currentProvince = cameFrom[currentProvince];
+                    path.Insert(0, currentProvince);
+                }
+                return path;
+            }
+
+            foreach ((ProvinceId neighbourProvince, uint distance) in currentProvince.Neighbours)
+            {
+                if (neighbourProvince.Province is not HabitableProvince) continue;
+                uint tentativeGScore = gScore[currentProvince] + distance;
+
+                if (gScore.TryGetValue(neighbourProvince, out uint value) && tentativeGScore >= value) continue;
+                cameFrom[neighbourProvince] = currentProvince;
+                value = tentativeGScore;
+                gScore[neighbourProvince] = value;
+
+                uint fScore = tentativeGScore + CalculateEuclideanHeuristic(neighbourProvince, endProvince);
+                openSet.Enqueue(neighbourProvince, fScore);
+            }
+        }
+        
+        return [];
+    }
+    
+    private static uint CalculateEuclideanHeuristic(ProvinceId current, ProvinceId goal)
+    {
+        return (uint)current.Centre.DistanceTo(goal.Centre);
     }
 }
