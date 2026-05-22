4e5eb7258b5a4f1f6f0f56ab0582e637df53d66e
Blake Davis <brickanimator@gmail.com>
2026-05-22 16:40:36 +1200

    Created RegionBuilder

---
+++ VictorianAnimalGame/Engine/Map/Regions/RegionBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using VictorianAnimalGame.Engine.Map.Cantons;
+
+namespace VictorianAnimalGame.Engine.Map.Regions;
+
+public class RegionBuilder
+{
+    private string _name;
+    private string _colour;
+    private HashSet<CantonId> _cantons;
+    
+    public RegionBuilder SetRegionName(string newName)
+    {
+        _name = newName;
+        return this;
+    }
+
+
+    public RegionBuilder SetRegionColour(string newColour)
+    {
+        _colour = newColour;
+        return this;
+    }
+
+    public RegionBuilder SetRegionCantons(List<string> newCantons, Dictionary<string, CantonId> cantonNames)
+    {
+        HashSet<CantonId> regionCantons = [];
+        foreach (string cantonName in newCantons)
+            regionCantons.Add(cantonNames[cantonName]);
+        _cantons = regionCantons;
+        return this;
+    }
+
+    public RegionDetails Build()
+    {
+        return new RegionDetails(_name, _colour, _cantons);
+    }
+}
