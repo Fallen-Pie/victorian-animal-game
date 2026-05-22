1bb400e42d1b9c8c039e3c898b85570ce506ffc5
Blake Davis <brickanimator@gmail.com>
2026-05-22 16:40:12 +1200

    Added RegionConfig to Yaml Reader

---
+++ VictorianAnimalGame/FileReader/DataReader/DataConfig/RegionConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace VictorianAnimalGame.FileReader.DataReader.DataConfig;
+
+public class RegionConfig : IDataConfig
+{
+    public RegionData Region { get; set; } 
+}
+
+public class RegionData
+{
+    public string Name { get; set; }
+    
+    public List<string> Cantons { get; set; }
+    
+    public string Colour { get; set; }
+}
