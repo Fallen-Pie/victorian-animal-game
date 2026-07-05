a81c1eb0310cbe82459ef433fcaf1c6c8ac709d0
Blake Davis <brickanimator@gmail.com>
2026-07-06 03:51:05 +1200

    Added Initial LabourAmount struct

---
+++ VictorianAnimalGame/Engine/Components/Production/Labour/LabourAmount.cs
@@ -0,0 +1,9 @@
+﻿using VictorianAnimalGame.Engine.Components.Critters.Occupations;
+
+namespace VictorianAnimalGame.Engine.Components.Production.Labour;
+
+public struct LabourAmount
+{
+    public OccupationType Occupation;
+    public uint Amount;
+}
